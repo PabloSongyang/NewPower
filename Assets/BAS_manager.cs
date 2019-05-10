b4a19b81603be4fe61ad2dc35d94c0e4a15c19bd
honda <honda_lau_freeself@outlook.com>
2019-05-10 10:17:39 +0800

    工程创建

---
+++ Assets/BAS_manager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LitJson;
+using UnityEngine.UI;
+
+public class BAS_manager : MonoBehaviour {
+
+    [SerializeField]
+    BAS_list precast;
+    [SerializeField]
+    Transform father;
+
+
+    public void setBAS_list(BAS_list list)
+    {
+        precast = list;
+    }
+
+
+    public void Getjson(JsonData json)
+    {
+        Debug.Log(JsonMapper.ToJson(json));
+        List<JsonData> listjson = new List<JsonData>();
+
+        JsonData data = json["data"];
+
+        for (int i = 0; i < data.Count; i++)
+        {
+            listjson.Add(data[i]);
+        }
+        Cleangglist();
+        foreach (JsonData i in listjson)
+        {
+            GameObject newobj = Instantiate(precast.gameObject);
+            newobj.transform.parent = father;
+            newobj.transform.localScale = new Vector3(1, 1, 1);
+            newobj.GetComponent<BAS_list>().json = i;
+            newobj.GetComponent<BAS_list>().Update_data();
+            newobj.SetActive(true);
+        }
+        StartCoroutine(delate());
+    }
+    public void Cleangglist()
+    {
+        for (int i = 0; i < father.childCount; i++)
+        {
+            Destroy(father.GetChild(i).gameObject);
+        }
+       
+    }
+
+
+
+    IEnumerator delate()
+    {
+
+        yield return new WaitForSeconds(0.1f);
+        father.GetComponent<ContentSizeFitter>().enabled = false;
+        yield return new WaitForSeconds(0.1f);
+        father.GetComponent<ContentSizeFitter>().enabled = true;
+    }
+
+}
---
+++ Assets/Scirpts/recharge_event.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class recharge_event : MonoBehaviour {
+
+    [SerializeField]
+    ToggleGroup group;
+    [SerializeField]
+    HttpModel http;
+
+
+    private void OnEnable()
+    {
+        if (Static.Instance.GetValue("zzflag") == "0")
+            group.gameObject.SetActive(false);
+        else
+            group.gameObject.SetActive(true);
+    }
+
+    public void Onclick()
+    {
+        if (Static.Instance.GetValue("zzflag") != "0")
+        {
+            IEnumerable<Toggle> toggleGroup = group.ActiveToggles();
+            foreach (Toggle t in toggleGroup)
+            {//遍历这个存放Toggle的按钮组IEnumerable，此乃C#的一个枚举集合，一般直接用foreach遍历
+                if (t.isOn)//遍历到一个被选择的Toggle
+                {
+                    Static.Instance.AddValue("ppflag", t.name);
+                }
+            }
+        }
+        
+
+        else
+            Static.Instance.AddValue("ppflag", "0");
+
+        http.Get();
+    }
+}
